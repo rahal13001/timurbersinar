70726aa2efa1fc7a86278aabf5f725cbcab6fc41
rahal13001 <rahal13001@mail.unpad.ac.id>
2023-07-20 13:21:18 +0900

    perbaikan routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/catalog';
+    public const HOME = '/pengelola/catalog';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
